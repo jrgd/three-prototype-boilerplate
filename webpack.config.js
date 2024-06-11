b0d4908c5b42ed871c37ed08134028d779ee3da4
jrgd <jrgd@mac.mini.studio>
2024-06-11 16:29:11 -0400

    take 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,56 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devtool: 'cheap-module-source-map',
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    'style-loader',
-                    'css-loader', 
-                    {
-                                loader: "postcss-loader",
-                                options: {
-                                  postcssOptions: {
-                                    plugins: [
-                                      [
-                                        "postcss-preset-env",
-                                        {
-                                          // Options
-                                        },
-                                      ],
-                                    ],
-                                  },
-                                }},],
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'cheap-module-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ['postcss-preset-env', {}],
+                ],
+              },
             },
-            {
-              test: /\.scss$/,
-              use: [{ loader: "style-loader" }, { loader: "css-loader" }, { loader: "sass-loader" }]
-            }, 
+          },
+          'sass-loader',
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-        new CopyPlugin({
-          patterns: [
-            {
-              from: "src/assets/",
-              to: "assets/",
-            },
-          ],
-        }),
+      },
     ],
-    devServer: {
-        static: './dist',
-        hot: true,
-    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/assets/',
+          to: 'assets/',
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
 };
